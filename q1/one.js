d5d5401c4833564ed0c739056db4cbb4fdf7ffe2
Archit Shankar <ashankar637@gmail.com>
2025-04-02 19:48:57 +0530

    new1

--- q1/one.js
+++ q1/one.js
@@ -1,74 +1,4 @@
 
-
-// const express = require('express');
-
-
-// const axios = require('axios');
-
-// const app = express();
-// const port = 9876;
-// const windowSize = 10;
-
-// const Url = 'http://20.244.56.144/evaluation-service';
-// let numberStore = [];
-
-// app.get('/numbers/:numberid', async (req, res) => {
-//   const { numberid } = req.params;
-//   const validIds = ['p', 'f', 'e', 'r'];
-  
-//   if (!validIds.includes(numberid)) {
-//     return res.status(400).json({ error: 'Invalid number ID' });
-//   }
-
-//   const apiEndpoint = {
-//     'p': 'primes',
-//     'f': 'fibo',
-//     'e': 'even',
-//     'r': 'rand'
-//   }[numberid];
-
-//   const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNTk5NjM0LCJpYXQiOjE3NDM1OTkzMzQsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImE4OTNkYzk2LWYzNzMtNDgwNS05MDk3LWIwY2JiNzY5NGJkOCIsInN1YiI6ImFzaGFua2FyNjM3QGdtYWlsLmNvbSJ9LCJlbWFpbCI6ImFzaGFua2FyNjM3QGdtYWlsLmNvbSIsIm5hbWUiOiJhcmNoaXRzaGFua2FyIiwicm9sbE5vIjoiMjIwNjE2NiIsImFjY2Vzc0NvZGUiOiJud3B3cloiLCJjbGllbnRJRCI6ImE4OTNkYzk2LWYzNzMtNDgwNS05MDk3LWIwY2JiNzY5NGJkOCIsImNsaWVudFNlY3JldCI6IkNqY1pmaGh6WWhqV2RCTUoifQ.9I-fkOLUDVT7MTBP1Lx-eQCQYo8CJNUMDBXPg4R2aKU";
-
-//   try {
-//     const windowPrevState = [...numberStore];
-//     const response = await axios.get(`${Url}/${apiEndpoint}`, { timeout: 500 ,
-//         headers: {
-//             'Authorization': `Bearer ${accessToken}`
-//           }
-//     });
-//     const newNumbers = response.data.numbers;
-
-//     newNumbers.forEach(num => {
-//       if (!numberStore.includes(num)) {
-//         if (numberStore.length >= windowSize) {
-//           numberStore.shift();
-//         }
-//         numberStore.push(num);
-//       }
-//     });
-
-//     const avg = numberStore.length >= windowSize 
-//       ? (numberStore.reduce((sum, num) => sum + num, 0) / windowSize).toFixed(2)
-//       : null;
-
-//     res.json({
-//       windowPrevState,
-//       windowCurrState: numberStore,
-//       numbers: newNumbers,
-//       avg: avg ? parseFloat(avg) : null
-//     });
-//   } catch (error) {
-//     if (error.code === 'ECONNABORTED') {
-//       res.status(504).json({ error: 'Request timeout' });
-//     } else {
-//       res.status(500).json({ error: 'Internal server error' });
-//     }
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 const express = require('express');
 const axios = require('axios');
 
@@ -80,7 +10,7 @@ const Url = 'http://20.244.56.144/evaluation-service';
 let numberStore = [];
 
 // Get the access token from your credentials
-const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNjAyMTUzLCJpYXQiOjE3NDM2MDE4NTMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImE4OTNkYzk2LWYzNzMtNDgwNS05MDk3LWIwY2JiNzY5NGJkOCIsInN1YiI6ImFzaGFua2FyNjM3QGdtYWlsLmNvbSJ9LCJlbWFpbCI6ImFzaGFua2FyNjM3QGdtYWlsLmNvbSIsIm5hbWUiOiJhcmNoaXRzaGFua2FyIiwicm9sbE5vIjoiMjIwNjE2NiIsImFjY2Vzc0NvZGUiOiJud3B3cloiLCJjbGllbnRJRCI6ImE4OTNkYzk2LWYzNzMtNDgwNS05MDk3LWIwY2JiNzY5NGJkOCIsImNsaWVudFNlY3JldCI6IkNqY1pmaGh6WWhqV2RCTUoifQ.Vjb_-XcGWwetnF5iB4ehM5zq728lairo_txl50AcdaI";
+const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNjAzNjMyLCJpYXQiOjE3NDM2MDMzMzIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImE4OTNkYzk2LWYzNzMtNDgwNS05MDk3LWIwY2JiNzY5NGJkOCIsInN1YiI6ImFzaGFua2FyNjM3QGdtYWlsLmNvbSJ9LCJlbWFpbCI6ImFzaGFua2FyNjM3QGdtYWlsLmNvbSIsIm5hbWUiOiJhcmNoaXRzaGFua2FyIiwicm9sbE5vIjoiMjIwNjE2NiIsImFjY2Vzc0NvZGUiOiJud3B3cloiLCJjbGllbnRJRCI6ImE4OTNkYzk2LWYzNzMtNDgwNS05MDk3LWIwY2JiNzY5NGJkOCIsImNsaWVudFNlY3JldCI6IkNqY1pmaGh6WWhqV2RCTUoifQ.Ri7WWCfhh92b3UPyrt-Pd03LKysjKX4vRyUtf04LRnc";
 
 app.get('/numbers/:numberid', async (req, res) => {
   const { numberid } = req.params;
